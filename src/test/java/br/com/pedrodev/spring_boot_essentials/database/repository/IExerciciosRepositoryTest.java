c8c7c09e7523664630e34a73294df3682a13ddd4
Pedro-Reis11 <pedroverissimoreis@gmail.com>
2026-05-24 14:40:34 -0300

    feat: ExerciciosRepository unit tests

---
+++ src/test/java/br/com/pedrodev/spring_boot_essentials/database/repository/IExerciciosRepositoryTest.java
@@ -0,0 +1,126 @@
+package br.com.pedrodev.spring_boot_essentials.database.repository;
+
+import br.com.pedrodev.spring_boot_essentials.database.model.ExerciciosEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class IExerciciosRepositoryTest {
+
+    @Autowired
+    private IExerciciosRepository repository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private ExerciciosEntity buildExcercicio(String nome, String grupoMuscular) {
+        return ExerciciosEntity.builder()
+                .nome(nome)
+                .grupoMuscular(grupoMuscular)
+                .build();
+    }
+
+    @BeforeEach
+    void setUp() {
+        repository.deleteAll();
+    }
+
+    @Nested
+    class FindByGrupoMuscular {
+        @Test
+        @DisplayName("Should find exercicio by grupo muscular")
+        void shouldFindExercicioByGrupoMuscular() {
+            //Arrange
+            repository.save(buildExcercicio("Supino", "Peito"));
+
+            //Act
+            var result = repository.findByGrupoMuscular("Peito");
+
+            //Assert
+            assertFalse(result.isEmpty());
+            assertEquals(1, result.size());
+            assertEquals("Supino", result.getFirst().getNome());
+            assertEquals("Peito", result.getFirst().getGrupoMuscular());
+        }
+
+        @Test
+        @DisplayName("Should return empty list when no exercicio found")
+        void shouldReturnEmptyListWhenNoExercicioFound() {
+            //Arrange
+            repository.save(buildExcercicio("Supino", "Peito"));
+
+            //Act
+            var result = repository.findByGrupoMuscular("Costas");
+
+            //Assert
+            assertTrue(result.isEmpty());
+        }
+
+        @Test
+        @DisplayName("Should return empty list when repository is empty")
+        void shouldReturnEmptyListWhenRepositoryIsEmpty() {
+            //Act
+            var result = repository.findByGrupoMuscular("Peito");
+
+            //Assert
+            assertTrue(result.isEmpty());
+        }
+    }
+
+    @Nested
+    class FindAllByOrderByGrupoMuscularAsc{
+        @Test
+        @DisplayName("Should find all exercicios ordered by grupo muscular ascending")
+        void shouldFindAllByOrderByGrupoMuscularAsc() {
+            //Arrange
+            repository.save(buildExcercicio("Supino", "Peito"));
+            repository.save(buildExcercicio("Remada", "Costas"));
+
+
+            //Act
+            var result = repository.findAllByOrderByGrupoMuscularAsc();
+
+            //Assert
+            assertFalse(result.isEmpty());
+            assertEquals(2, result.size());
+            assertEquals("Costas", result.getFirst().getGrupoMuscular());
+            assertEquals("Peito", result.getLast().getGrupoMuscular());
+        }
+
+        @Test
+        @DisplayName("Should return single exercicio when only one exists")
+        void shouldReturnSingledExercicioWhenOnlyOneExists() {
+            //Arrange
+            repository.save(buildExcercicio("Supino", "Peito"));
+
+            //Act
+            var result = repository.findAllByOrderByGrupoMuscularAsc();
+
+            //Assert
+            assertFalse(result.isEmpty());
+            assertEquals(1, result.size());
+            assertEquals("Supino", result.getFirst().getNome());
+        }
+
+        @Test
+        @DisplayName("Should return empty list when repository is empty")
+        void shouldReturnEmptyListWhenRepositoryIsEmpty() {
+            //Act
+            var result = repository.findAllByOrderByGrupoMuscularAsc();
+
+            //Assert
+            assertTrue(result.isEmpty());
+        }
+    }
+
+
+}
