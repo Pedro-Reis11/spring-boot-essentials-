01e2a4a83f53fe25155db9fbacd69b45e2bdd840
Pedro-Reis11 <pedroverissimoreis@gmail.com>
2026-05-29 19:58:03 -0300

    feat: Integration Test for TreinosController

--- src/test/java/br/com/pedrodev/spring_boot_essentials/integration/TreinosControllerIT.java
+++ src/test/java/br/com/pedrodev/spring_boot_essentials/integration/TreinosControllerIT.java
@@ -6,6 +6,7 @@ import br.com.pedrodev.spring_boot_essentials.database.repository.IAlunosReposit
 import br.com.pedrodev.spring_boot_essentials.database.repository.IExerciciosRepository;
 import br.com.pedrodev.spring_boot_essentials.database.repository.ITreinosRepository;
 import br.com.pedrodev.spring_boot_essentials.dto.TreinoDto;
+import br.com.pedrodev.spring_boot_essentials.exception.ErrorResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
@@ -115,7 +117,8 @@ class TreinosControllerIT {
                 "/treinos/{idAluno}/treinos",
                 HttpMethod.GET,
                 null,
-                new ParameterizedTypeReference<List<TreinoDto>>() {},
+                new ParameterizedTypeReference<List<TreinoDto>>() {
+                },
                 aluno.getId());
 
         assertThat(response).isNotNull();
@@ -131,11 +134,11 @@ class TreinosControllerIT {
                 "/treinos/99/treinos",
                 HttpMethod.GET,
                 null,
-                TreinoDto.class);
+                ErrorResponse.class);
 
         assertThat(response).isNotNull();
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-        assertThat("Aluno não encontrado").isNotNull();
+        assertThat(response.getBody().getMessage()).isEqualTo("Aluno não encontrado");
     }
 
     //POST/treinos
@@ -163,11 +166,151 @@ class TreinosControllerIT {
 
         testRestTemplate.postForEntity("/treinos", dto, TreinoDto.class);
 
-        var response = testRestTemplate.postForEntity("/treinos", dto, TreinoDto.class);
+        var response = testRestTemplate.postForEntity("/treinos", dto, ErrorResponse.class);
 
         assertThat(response).isNotNull();
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(response.getBody().getMessage()).isEqualTo("Treino já existe para esse aluno");
         assertThat(repository.count()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("Should return 404 when aluno not found to create treino")
+    void shouldReturn404WhenAlunoNotFoundToCreate() {
+        var dto = buildTreinoDto(99, "Treino A", Set.of(exercicios.getId()));
+
+        var response = testRestTemplate.postForEntity("/treinos", dto, ErrorResponse.class);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody().getMessage()).isEqualTo("Aluno não encontrado");
+        assertThat(repository.count()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("Should return 404 when exercicio not found to create treino")
+    void shouldReturn404WhenExercicioNotFoundToCreate() {
+        var dto = buildTreinoDto(aluno.getId(), "Treino A", Set.of(99));
+
+        var response = testRestTemplate.postForEntity("/treinos", dto, ErrorResponse.class);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody().getMessage()).isEqualTo("Exercício com id "+dto.getExerciciosIds().iterator().next()+" não encontrado");
+        assertThat(repository.count()).isZero();
+    }
+
+    //Put/treinos/{id}
+    @Test
+    @DisplayName("Should update a treino and return updated treino")
+    void shouldUpdateATreinoAndReturnUpdatedDto() {
+        var dto = buildTreinoDto(aluno.getId(), "Treino A", Set.of(exercicios.getId()));
+        testRestTemplate.postForEntity("/treinos", dto, TreinoDto.class);
+
+        var treino = repository.findByNomeAndAlunoId("Treino A", aluno.getId()).orElseThrow();
+
+        var response = testRestTemplate.exchange("/treinos/{id}",
+                HttpMethod.PUT,
+                new HttpEntity<>(buildTreinoDto(aluno.getId(), "Treino A - Updated", Set.of(exercicios.getId()))),
+                TreinoDto.class,
+                treino.getId());
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getNome()).isEqualTo("Treino A - Updated");
+    }
+
+    @Test
+    @DisplayName("Should return 404 when treino not found to update")
+    void shouldReturn404WhenTreinoNotFoundToUpdate() {
+        var response = testRestTemplate.exchange("/treinos/99",
+                HttpMethod.PUT,
+                new HttpEntity<>(buildTreinoDto(aluno.getId(), "Treino A", Set.of(exercicios.getId()))),
+                ErrorResponse.class);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody().getMessage()).isEqualTo("Treino não encontrado para esse aluno");
+    }
+
+    @Test
+    @DisplayName("Should return 404 when one or more exercicios not found on update")
+    void shouldReturn404WhenOneOrMoreExerciciosNotFoundOnUpdate() {
+        testRestTemplate.postForEntity("/treinos",
+                buildTreinoDto(aluno.getId(), "Treino A", Set.of(exercicios.getId())),
+                TreinoDto.class);
+
+        var treino = repository.findByNomeAndAlunoId("Treino A", aluno.getId()).orElseThrow();
+
+        var response = testRestTemplate.exchange("/treinos/{id}",
+                HttpMethod.PUT,
+                new HttpEntity<>(buildTreinoDto(aluno.getId(), "Treino A - Updated", Set.of(99))),
+                ErrorResponse.class,
+                treino.getId());
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody().getMessage()).isEqualTo("Um ou mais exercícios não foram encontrados");
+        assertThat(treino.getNome()).isEqualTo("Treino A");
+    }
+
+    //Delete/treinos/alunos/{idAluno}/treinos/{idTreino}
+
+    @Test
+    @DisplayName("Should delete a treino with success")
+    void shouldDeleteATreinoWithSuccess() {
+        testRestTemplate.postForEntity("/treinos",
+                buildTreinoDto(aluno.getId(), "Treino A", Set.of(exercicios.getId())),
+                TreinoDto.class);
+        //pegar o id para deletar
+        var treino = repository.findByNomeAndAlunoId("Treino A", aluno.getId()).orElseThrow();
+
+        var response = testRestTemplate.exchange("/treinos/alunos/{idAluno}/treinos/{idTreino}",
+                HttpMethod.DELETE,
+                null,
+                Void.class,
+                treino.getAluno().getId(),
+                treino.getId());
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+        assertThat(repository.findById(treino.getId())).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Should return 404 when treino not found to delete")
+    void shouldReturn404WhenTreinoNotFoundToDelete() {
+        var response = testRestTemplate.exchange("/treinos/alunos/{idAluno}/treinos/{idTreino}",
+                HttpMethod.DELETE,
+                null,
+                ErrorResponse.class,
+                aluno.getId(),
+                99);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody().getMessage()).isEqualTo("Treino não encontrado para esse aluno");
+    }
+
+    @Test
+    @DisplayName("Should return 404 when aluno not found to delete")
+    void shouldReturn404WhenAlunoNotFoundToDelete() {
+        testRestTemplate.postForEntity("/treinos",
+                buildTreinoDto(aluno.getId(), "Treino A", Set.of(exercicios.getId())),
+                TreinoDto.class);
+        var treino = repository.findByNomeAndAlunoId("Treino A", aluno.getId()).orElseThrow();
+
+        var response = testRestTemplate.exchange("/treinos/alunos/{idAluno}/treinos/{idTreino}",
+                HttpMethod.DELETE,
+                null,
+                ErrorResponse.class,
+                99,
+                treino.getId());
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody().getMessage()).isEqualTo("Treino não encontrado para esse aluno");
     }
 }
 
